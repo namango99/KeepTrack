2a73c348880a793b64420cdc0fe7c78e9650bac1
Naman Goyal <namango99@gmail.com>
2021-01-13 13:41:00 +0530

    Added Individual Time for different URLS

--- background.js
+++ background.js
@@ -17,7 +17,7 @@ function giveDomain(URL) {
         for(iter = 0; iter < current.length; iter++){
             if(current[iter] ===  URL[iter]){
                 continue;
-            }else {
+            }else { 
                 ok = false;
             }
         }
@@ -42,15 +42,31 @@ var value = setInterval(function(){
     chrome.tabs.query({active:true, lastFocusedWindow:true}, function(tab){
         if(tab[0] != undefined){
             var url = tab[0].url;
+            var ok = true;
+            for(var i = 0; i < url.length; i++){
+                if(url[i] != "\xa0"){
+                    ok = false;
+                    break;
+                }
+            }
+            if(ok) {
+                return;
+            }
             var domain_ = giveDomain(url);
             if(domain_ != undefined){
                 var get = localStorage.getItem(domain_);
                 if(get == null){
-                    localStorage.setItem(domain_, 5);
+                    var Array = makeArray();
+                    updateData(Array, giveSlotNumber());
+                    localStorage.setItem(domain_, dataToString(Array));
                 }else {
+                    // console.log(get);
+                    var Array = splitArray(get);
+                    // console.log(Array)
+                    updateData(Array, giveSlotNumber());
+                    // console.log(Array)
                     localStorage.removeItem(domain_);
-                    var value = Number(get) + Number(5);
-                    localStorage.setItem(domain_, value);
+                    localStorage.setItem(domain_, dataToString(Array));
                 }
             }
         }
@@ -65,3 +81,44 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         sendResponse("Done");
     }
 })
+
+function giveSlotNumber() {
+    var time = new Date();
+    var hrs = time.getHours();
+    return hrs + 1;
+}
+
+function makeArray(){
+    var ans = [];
+    ans.push(0);
+    for(var i = 0; i < 24; i++){
+        ans.push([i,0]);
+    }
+    return ans;
+}
+
+function dataToString(arr){
+    var toString = [];
+    toString.push(arr[0]);
+    for(var i = 1; i < arr.length; i++){
+        toString.push(arr[i][0] + "," + arr[i][1]);
+    }
+    // console.log(toString);
+    return toString.join("|");
+}
+
+function splitArray(data){
+    var splitted = data.split("|");
+    splitted[0] = Number(splitted[0]);
+    for(var i = 1; i < splitted.length; i++){
+        var now = splitted[i].split(",");
+        splitted[i] = [Number(now[0]), Number(now[1])]
+    }
+    return splitted;
+}
+
+function updateData(data, slotNumber){
+    // console.log(data + " " + slotNumber + " " + data[slotNumber])
+    data[0] += 5;
+    data[slotNumber][1] += 5;
+}
--- popup.js
+++ popup.js
@@ -1,16 +1,3 @@
-var clearDataBtn = document.querySelector("button");
-
-clearDataBtn.addEventListener("click", function(){
-    clearData();
-    var allData = document.querySelectorAll("td");
-    for(var i = 0; i < allData.length; i++){
-        allData[i].parentNode.removeChild(allData[i]);
-    }
-    console.log("Data Cleared! and Extension Restarted..")
-    var span = document.querySelector("span");
-    span.innerHTML = 0 + " hours " + 0 + " minutes " + 0 + " seconds "; 
-})
-
 getData();
 
 function getData(){
@@ -21,8 +8,8 @@ function getData(){
         var sortable = []
         for(var i = 0; i < keys.length; i++){
             var keyData = keys[i];
-            var actualData = response[keyData];
-            sortable.push([keyData, actualData]);
+            var actualData = splitArray(response[keyData])[0];
+            sortable.push([keyData, actualData, splitArray(response[keyData])]);
             overAllTime += Number(actualData)
         }
         sortable.sort(function(a, b){
@@ -31,16 +18,82 @@ function getData(){
         for(var i = 0; i < sortable.length; i++){
             var keyData = sortable[i][0]
             var actualData = sortable[i][1]
-            var table = document.querySelector("table");
-            var row = table.insertRow(i);
+            var data = document.querySelector("body");
             var timeInSeconds = Number(actualData);
             var time = convertTime(timeInSeconds);
-            row.innerHTML = "<td>" + keyData + "</td>" + "<td class=\"left_assign\">" + time.hours+ " hours </td>" + "<td class=\"left_assign\">" + time.minutes+ " minutes </td>" + "<td class=\"left_assign\">" + time.seconds+ " seconds </td>";
+            var newData = document.createElement("h3");
+            newData.style = "clear: both;";
+            // + "<div>" + innerData(sortable[i][2]) + "</div>"
+            newData.innerHTML = "<span class=\"space leftAlign\">" + keyData + "</span>" + makeButton(i) + "<span class=\"space rightAlign\"> " + time.hours+ " hours " + "   " + time.minutes+ " minutes" + "     " + time.seconds+ " seconds </span>" 
+            data.appendChild(newData);
+            var newDataChild = document.createElement("div");
+            newDataChild.style = "clear: both;";
+            newDataChild.innerHTML = innerDataUpdated(sortable[i][2], i);
+            data.appendChild(newDataChild);
+
+            var btn = ".toggle" + i;
+            var tab = ".hide" + i;
+            var toggleBtn = document.querySelector(btn);
+            toggleBtn.myParam = "" + tab;
+            toggleBtn.addEventListener("click", function(event) {
+                var giveTab = event.currentTarget.myParam;
+                var tab_ = document.querySelector(giveTab);
+                tab_.classList.toggle("hide");
+                // fa-caret-down
+                // fa-sort-up
+                var iconId = ".icon" + giveTab.slice(5, giveTab.length);
+                var icontag = document.querySelector(iconId);
+                if(icontag.style.transform == "") {
+                    // console.log("ROTATING");
+                    icontag.style.transform = "rotate(180deg)"
+                }else {
+                    // console.log("REVERSE ROTATING");
+                    icontag.style.transform = "";
+                }
+                // console.log(giveTab + " " + iconId);
+                // if(icontag.classList.contains("fa-caret-down")) {
+                //     icontag.classList.remove("fa-caret-down");
+                //     icontag.classList.add("fa-sort-up");
+                // }else {
+                //     icontag.classList.remove("fa-sort-up");
+                //     icontag.classList.add("fa-caret-down");
+                // }
+            })
         }
         var span = document.querySelector("span");
+        span.className = "bigSize";
         var time = convertTime(overAllTime);
-        span.innerHTML = time.hours + " hours " + time.minutes + " minutes " + time.seconds + " seconds "; 
+        span.innerHTML = "\xa0\xa0\xa0" + time.hours + " hours " + time.minutes + " minutes "; 
+
+        var body = document.querySelector("body");
+        var newData = document.createElement("button");
+        newData.className = "clear";
+        newData.innerHTML = "Clear Data";
+        body.appendChild(newData);
+
+        var clearDataBtn = document.querySelector(".clear");
+
+        clearDataBtn.addEventListener("click", function(){
+            clearData();
+            var allData = document.querySelectorAll("div");
+            for(var i = 0; i < allData.length; i++){
+                allData[i].parentNode.removeChild(allData[i]);
+            }
+            allData = document.querySelectorAll("h3");
+            for(var i = 0; i < allData.length; i++){
+                allData[i].parentNode.removeChild(allData[i]);
+            }
+            console.log("Data Cleared! and Extension Restarted..")
+            var span = document.querySelector("span");
+            span.innerHTML = 0 + " hours " + 0 + " minutes " + 0 + " seconds "; 
+        })
+
     })
+}
+
+function makeButton(number){
+    var data = "<button class=\"toggle" + number + "\">" + "<i class=\"fas icon" + number +  " fa-caret-down\"></i>" + "</button>"
+    return data;
 }
 
 function clearData() {
@@ -55,4 +108,45 @@ function convertTime(timeInSeconds) {
     timeInSeconds -= mins * 60;
     var secs = timeInSeconds;
     return {"hours":hrs, "minutes":mins, "seconds":secs};
+}
+
+function splitArray(data){
+    var splitted = data.split("|");
+    splitted[0] = Number(splitted[0]);
+    for(var i = 1; i < splitted.length; i++){
+        var now = splitted[i].split(",");
+        splitted[i] = [Number(now[0]), Number(now[1])]
+    }
+    return splitted;
+}
+
+function innerData(data){
+//     var html = ["<div>"]
+    var html = []
+    for(var i = 1; i < data.length; i++){
+        var start = i - 1, end = i;
+        var time = convertTime(data[i][1]);
+        html.push("<div style=\"clear: both;\"><span class=\"leftAlign\">" + start + "\xa0\xa0\xa0-\xa0\xa0\xa0" + end + "</span><span class=\"rightAlign\">" + time.hours+ "\xa0\xa0\xa0hours\xa0\xa0\xa0" + time.minutes+ "\xa0\xa0\xa0minutes\xa0\xa0\xa0" + time.seconds+ "\xa0\xa0\xa0seconds\xa0\xa0\xa0</span></div>")
+    }
+    html.push("<div><i class=\"fas fa-chevron-circle-up leftAlign\"></i></div>")
+    // html.push("</div>")
+    return html.join("");
+}
+
+function innerDataUpdated(data, number) { 
+    var html = ["<table class=\"hide hide" + number + "\">"]
+    html.push("<thead>")
+    html.push("<tr>")
+    html.push("<td>START</td><td>END</td><td>HRS</td><td>MINS</td><td>SECS</td>")
+    html.push("</tr></thead>")
+    html.push("<tbody>")
+    for(var i = 1; i < data.length; i++){
+        html.push("<tr>")
+        var time = convertTime(data[i][1])
+        html.push("<td>" + (i - 1) + "</td><td>" + i + "</td><td>" + time.hours + "</td><td>" + time.minutes + "</td><td>" + time.seconds + "</td>")
+        html.push("</tr>")
+    }
+    html.push("</tbody>")
+    html.push("</table>");
+    return html.join("");
 }
